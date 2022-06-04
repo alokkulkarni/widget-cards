e6590578832187d1202d56c4225e7e497466788f
Alok Kulkarni <kulkarni.alok@gmail.com>
2022-06-04 20:29:32 +0100

    added new comment

--- pages/index.js
+++ pages/index.js
@@ -79,6 +79,19 @@ export default function Home({ cards }) {
           ))}
         </div>
       </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer> 
     </div>
   )
 }
@@ -152,17 +165,3 @@ export async function getStaticProps() {
               </div>
             ))}
         </div> */}
-
-
-             {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
